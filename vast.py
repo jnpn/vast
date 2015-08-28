6efa33eac9f758aad34b856ca4c2fe60edb787c0
Johan PONIN <johan.ponin.pro@gmail.com>
2015-08-28 02:36:05 +0200

    Source class wrap source and related functions

--- vast.py
+++ vast.py
@@ -9,48 +9,63 @@ cases not handled yet.
 import ast
 from subprocess import Popen, PIPE
 
-from visitors import Elispy, Dummy, Generic, Meta
+from visitors import Generic, Meta, Elispy
 from snippets import snippets
 
-def debug(s):
-    '''Parse source then pass the ast to the Dummy visitor.'''
-    a = ast.parse(s)
-    v = Dummy()
-    v.visit(a)
+class Source:
 
-def quotecode(source):
-    return '''```
+    '''
+    Packs source related methods into a single unit.
+    '''
+
+    def __init__(self, fn, source):
+        self.fn = fn
+        self.source = source
+
+    def talk(self):
+        return list(ast.walk(ast.parse(self.source)))
+
+    def elispy(self):
+        ''' parse s then pass it to the Elispy visitor '''
+        return Elispy().visit(ast.parse(self.source))
+
+    def debug(self):
+        '''Parse source then pass the ast to the Dummy visitor.'''
+        a = ast.parse(self.source)
+        v = Generic()
+        v.visit(a)
+
+    def quotecode(self, code):
+        return '''```
 %s
 ```
-''' % source
+''' % code
+
+    def emacs_eval(source):
+        '''Submit pretty printed src (from the visitor) to `emacs` for evaluation.'''
+
+        assert Popen(['which', 'emacs'], stdout=PIPE, stderr=PIPE).stderr is not b''
+
+        wrapper = '(message "%%S" (progn %s))'
+        # http://www.emacswiki.org/emacs/BatchMode
+        emacs = Popen(['emacs','-batch','--eval', wrapper % self.source]
+                      , stderr=PIPE
+                      , stdout=PIPE)
+        return emacs.stderr.read().decode('utf8')
+
+    def transpile(self, visitor):
+        qs = self.quotecode('## %s (Python|source)\n' % self.fn + self.source)
+        tgt = visitor().visit(ast.parse(self.source))
+        qt = self.quotecode(';; %s (Lisp|target)\n' % self.fn + '\n' + tgt + '\n')
+        return qs, qt
 
 def premain(visitor):
     '''Parse test snippets and pass them to visitor.'''
     for name, source in snippets.items():
-        fn = name.capitalize()
-        print(quotecode('## %s (Python|source)\n' % fn + source))
-        target = visitor().visit(ast.parse(source))
-        print(quotecode(';; %s (Lisp|target)\n' % fn + '\n' + target + '\n'))
+        qs, qt = Source(name.capitalize(), source).transpile(visitor)
+        print(qs)
+        print(qt)
         print()
-
-def emacs_eval(elisp):
-    '''Submit pretty printed src (from the visitor) to `emacs` for evaluation.'''
-
-    assert Popen(['which', 'emacs'], stdout=PIPE, stderr=PIPE).stderr is not b''
-
-    wrapper = '(message "%%S" (progn %s))'
-    # http://www.emacswiki.org/emacs/BatchMode
-    emacs = Popen(['emacs','-batch','--eval', wrapper % elisp]
-                  , stderr=PIPE
-                  , stdout=PIPE)
-    return emacs.stderr.read().decode('utf8')
-
-def talk(s):
-    return list(ast.walk(ast.parse(s)))
-
-def elispy(s):
-    ''' parse s then pass it to the Elispy visitor '''
-    return Elispy().visit(ast.parse(s))
 
 def main():
     '''Helper, calls premain(Elispy).'''
